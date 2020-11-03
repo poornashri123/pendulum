08631c652b4887e89fe0e744a19f22703a0104f8
poornashri123 <59368796+poornashri123@users.noreply.github.com>
2020-11-03 16:50:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+  ellipse =createSprite(400,300,20)
+}
+
+function draw() {
+  background(338);  
+  drawSprites();
+}
